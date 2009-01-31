c182626c296d3ecae3932d05c00c7b39cd7aae04
Rob Hunter <rob.hunter@gmail.com>
2009-01-31 15:50:00 -0800

    production settings for data; correct web url

--- settings-prod.scm
+++ settings-prod.scm
@@ -3,4 +3,5 @@
 (setting-set! *PORT* 1123)
 ;; use #f if you want to listen to all incoming IPs:
 (setting-set! *LISTEN_IP* #f)
-(setting-set! *WEB_APP_URL* "http://lawnelephant.com:1123/")
+(setting-set! *WEB_APP_URL* "http://lawnelephant.com/")
+(setting-set! *PATH_TO_DATA* "/home/rob/le-prod-code/prod-data")
